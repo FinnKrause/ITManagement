eab31e659cfb01e098678fed57132690b7e566b3
Hendrik <hendrik@faustud-010-021-043-223.pool.uni-erlangen.de>
2025-11-04 18:08:46 +0100

    DatenbereinigungV2 Hendrik

--- Hendriks-Playground/datenbereinigung.py
+++ Hendriks-Playground/datenbereinigung.py
@@ -12,8 +12,24 @@ df = df.drop(columns=["zero"])
 #check hat funktioniert
 
 
-#Jedes Tupel, welches im dritten Attribut „purchase“ stehen hat, lässt sich restlos aus anderen Tupeln ableiten und bietet daher keinen Mehrwert für die Analyse 
-df = df[df["behavior"] == "play"]
+def count_purchase_play(df):
+    anzahl_purchase = (df["behavior"] == "purchase").sum()
+    anzahl_play = (df["behavior"] == "play").sum()
+    return anzahl_purchase, anzahl_play
 
-#anzeigen lassen
-print(df["behavior"].unique())
+# Anwendung:
+anz_purchase, anz_play = count_purchase_play(df)
+print("Anzahl 'purchase'-Zeilen:", anz_purchase)
+print("Anzahl 'play'-Zeilen:", anz_play)
+print (anz_play + anz_purchase)
+
+def filter_by_game(df, game_name):
+    return df[df["game"] == game_name]
+
+spiel_df = filter_by_game(df, "Hyperdimension Neptunia Re;Birth1")
+
+unique_users = df["user_id"].nunique()
+print("\nAnzahl verschiedener User:", unique_users)
+
+unique_games = df["game"].nunique()
+print("Anzahl verschiedener Spiele:", unique_games)
